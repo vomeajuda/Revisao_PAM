54b17132a13cee2ac5ca91e3033385590d89a2da
vomeajuda <leogugo9@hotmail.com>
2025-08-08 12:03:34 -0300

    done

--- App.js
+++ App.js
@@ -1,71 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import LoginScreen from './screens/loginScreen.js'
 
 export default function App() {
   return (
-    <View>
-      <View style={styles.container}>
-        <Image source={require('./assets/casual_dog.png')} style={styles.imagem}></Image>
-        <Text style={styles.titulo}>Ótimo dia!</Text>
-        <Text style={styles.subtitulo}>Como deseja acessar?</Text>
-      </View>
-      <View style={styles.container2}>
-          <Pressable style={styles.botao}>
-            <Image source={require('./assets/Google.png')} style={styles.google}></Image>
-            <Text style={{color: 'white'}}>Como deseja acessar?</Text>
-          </Pressable>
-          <Pressable style={styles.botao2}>
-            <Text>Outras opções</Text>
-          </Pressable>
-      </View>
-    </View>
+    <LoginScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 200,
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  container2: {
-    paddingTop: 150,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  titulo: {
-    paddingTop: 50,
-    fontSize: 40,
-  },
-  subtitulo: {
-    paddingTop: 10,
-    fontSize: 15
-  },
-  botao: {
-    flexDirection: 'row',
-    width: 430,
-    height: 70,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'lightgreen',
-    borderRadius: 8,
-    borderColor: 'black',
-    borderWidth: 0.5
-  },
-  botao2: {
-    marginTop: 20,
-    width: 430,
-    height: 70,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 8,
-    borderColor: 'black',
-    borderWidth: 0.5
-  },
-  google: {
-  }
-});
